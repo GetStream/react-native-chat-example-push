6ea5107b21f4110e62b7e893d7bdc7f6e6c0a8b3
Luke Smetham <luke@arkitek.co.uk>
2019-04-17 15:30:54 +0100

    eslint/prettier

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
